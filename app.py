07cb52cdb3465880e7876c13c9195351a4c1b162
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-09-18 10:57:36 -0500

    added a siple loop wikl make it work

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 # will write code here after the commit is done
+
+while True:
+    menu = ["1. HELP ME UNDERSTAND!"] # will be looking like empty and i will keep on adding when i add new features
+
+    for i in menu:
+        print(i)
+    while True:
+        try:
+            choice = int(input("Choose a number: "))
+            break
+        except Exception:
+            print("Please input a number from the menu")
+            continue
+    print("You choose ", choice)
