dea118fd7e58325b53fcbb57fb4a420ebf1e3150
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-09-18 11:13:34 -0500

    perfected the loop

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 # will write code here after the commit is done
 
-while True:
-    menu = ["1. HELP ME UNDERSTAND!"] # will be looking like empty and i will keep on adding when i add new features
+print("---MENU---")
+menu = ["1. HELP ME UNDERSTAND!", "2. exit"] # will be looking like empty and i will keep on adding when i add new features
 
-    for i in menu:
-        print(i)
+for i in menu:
+    print(i)
+
+while True:
+    
     while True:
         try:
             choice = int(input("Choose a number: "))
+            if choice == 2:
+                print("\nStay Wild!")
+                exit()
+            print(f"You choose the option {menu[choice -1]}")
             break
         except Exception:
             print("Please input a number from the menu")
             continue
-    print("You choose ", choice)
+
+        
